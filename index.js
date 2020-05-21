55a42f262e692a6affd6bb51b60091d2db77c113
Josh Luerssen <joshluer@gmail.com>
2020-05-21 08:20:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,16 +10,59 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = {
+    itemName: item,
+    itemPrice: Math.floor(Math.random() * 100) + 1
+  };
+  
+  cart.push(newItem);
+  return (`${item} has been added to your cart.`);
 }
 
 function viewCart() {
-  // write your code here
+  var l = cart.length;
+
+  if(!l) {
+    return ("Your shopping cart is empty.");
+  }
+  
+  let itemsAndPrices = [];
+    for (let i = 0; i < l; i++) {
+      let itemAndPrice = cart[i];
+      let item = itemAndPrice.itemName;
+      let price = itemAndPrice.itemPrice;
+  
+      itemsAndPrices.push(`${item} at $${price}`);
+    }
+
+  switch(itemsAndPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemsAndPrices = itemsAndPrices.join(", and ");
+      break;
+    default:
+      itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+      itemsAndPrices = itemsAndPrices.join(", ");
+  }
+
+  return (`In your cart, you have ${itemsAndPrices}.`);
+
 }
 
 function total() {
-  // write your code here
+  let t = 0;
+
+  for (let i = 0, l = cart.length; i < l; i++) {
+    for (let itemPrice in cart[i]) {
+      t += cart.itemPrice;
+    }
+  }
+  return t;
 }
+//function total() {
+  // write your code here
+//}
 
 function removeFromCart(item) {
   // write your code here
